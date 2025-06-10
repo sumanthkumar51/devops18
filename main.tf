bedc780a9ec26e2fbc898ce11a0899e348e7a2e6
Sumanth Kumar <45727189+sumanthkumar51@users.noreply.github.com>
2025-06-10 15:31:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ resource "aws_launch_template" "web_server_as" {
     image_id           = "ami-02457590d33d576c3"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t2.micro"
-    key_name = "lastone"
     tags = {
         Name = "DevOps"
     }
