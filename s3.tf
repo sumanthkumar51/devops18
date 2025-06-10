1bccb259912d4d48c3cb6779bed1d0cdc7fe45e8
Sumanth Kumar <45727189+sumanthkumar51@users.noreply.github.com>
2025-06-10 15:32:39 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "mustafashaik77.devops.project.bucket"
+  bucket = "demo490.k8s.local"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -25,7 +25,7 @@ status = "Enabled"
 terraform {
 backend "s3" {
 region = "us-east-1"
-bucket = "mustafashaik77.devops.project.bucket"
+bucket = "demo490.k8s.local"
 key = "prod/terraform.tfstate"
 }
 }
